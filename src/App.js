dbaa29506e86ca8903dab542387d6f195317a9ca
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-02-09 13:37:31 -0800

    form controls

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 
 function App() {
-  const name = 'John';
+  const [tasks, setTasks] = useState([
+    {
+      "id": 1,
+      "text": "Doctors Appointment",
+      "day": "Feb 5th at 2:30pm",
+      "reminder": true
+    },
+    {
+      "id": 2,
+      "text": "Meeting at School",
+      "day": "Feb 6th at 1:30pm",
+      "reminder": true
+    }
+  ])
+
+  // add task
+  const addTask = (task) => {
+    console.log(task);
+  }
+
+  // delete a task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // toggle reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task ))
+  }
 
   return (
     <div className="container">
-      <Header title={1} />
+      <Header />
+      <AddTask onAdd={addTask} />
+      { (tasks.length > 0) ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No tasks to show') }
     </div>
   );
 }
