57a4cd760ca4b939f5701bb5c073236c08ca10d3
DESKTOP-HOSGHCR\johnc <jrcianc@gmail.com>
2021-02-09 15:36:36 -0800

    added json-server

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -24,11 +24,19 @@ const AddTask = ({ onAdd }) => {
         <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)}/>
+                <input 
+                    type='text' 
+                    placeholder='Add Task' 
+                    value={text} 
+                    onChange={(e) => setText(e.target.value)}/>
             </div>
             <div className='form-control'>
                 <label>Date and time</label>
-                <input type='text' placeholder='Add Date and Time' value={day} onChange={(e) => setDay(e.target.value)}/>
+                <input 
+                    type='text' 
+                    placeholder='Add Date and Time' 
+                    value={day} 
+                    onChange={(e) => setDay(e.target.value)}/>
             </div>
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
